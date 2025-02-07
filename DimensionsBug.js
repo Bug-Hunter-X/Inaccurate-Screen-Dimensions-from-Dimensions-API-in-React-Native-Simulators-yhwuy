d0a71159917104a92126bcecb7d252daa91d1e38
BugHunter-X <askzenai@gmail.com>
2025-02-07 07:14:04 +0100

    Add DimensionsBug.js file

---
+++ DimensionsBug.js
@@ -0,0 +1,1 @@
+This error occurs when using the `Dimensions` API in React Native to get screen dimensions and the app is running in a simulator or emulator with a different screen size than the physical device. The dimensions returned might be inaccurate, leading to layout issues.
